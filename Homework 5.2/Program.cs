8da10556be713c6a378c73e1074e758fd7753eb1
Vladyslav1811 <vladkiriluk16@gmail.com>
2024-10-24 23:12:17 +0200

    Add files via upload

---
+++ Homework 5.2/Program.cs
@@ -0,0 +1,22 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.Write("Enter a number: ");
+            string? input = Console.ReadLine();
+
+            try
+            {
+                double number = double.Parse(input);
+                Console.WriteLine($"You entered: {number}");
+                break;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Error: invalid format. Please try again.");
+            }
+        }
+    }
+}
